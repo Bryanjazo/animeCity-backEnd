d8e4a2dfda7daa14f685fb411a5d25260c69713a
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-15 19:16:52 -0700

    added user create

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,5 +1,10 @@
 class AnimesController < ApplicationController
   def create
     anime = Anime.new(title: params[:title], episodes: params[:episodes], url: params[:url], image_url: params[:image_url], description: params[:description], genre: params[:genre])
+    if anime.save
+      render json: anime
+    else
+      render json: {message: "something went wrong"}
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: {message: "error"}
+    end
+  end
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password)
+  end
 end
