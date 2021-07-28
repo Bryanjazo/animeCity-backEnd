c555b2ef69ae66447af7a6de2628fe464c07fea3
Bryan Jazo <bryanjazo88@gmail.com>
2021-07-27 19:05:39 -0700

    built in update feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
   validates :email, presence: true
-  validates :password, presence: true
+
   has_many :watches
 
 end
